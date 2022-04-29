8321b2d1a66db21ff0ed035b12d9b0af119c4716
docw28 <d.carverwilliams@gmail.com>
2022-04-29 15:35:10 +0100

    Added jump animation and cleaned up collision with ground

--- index.js
+++ index.js
@@ -53,6 +53,14 @@ const player = new Fighter({
     run: {
       imageSrc: "./img/samuraiMack/Run.png",
       framesMax: 8
+    },
+    jump: {
+      imageSrc: "./img/samuraiMack/jump.png",
+      framesMax: 2
+    },
+    fall: {
+      imageSrc: "./img/samuraiMack/fall.png",
+      framesMax: 2
     }
   }
 });
@@ -95,14 +103,21 @@ function animate() {
 
   //player movement
   player.velocity.x = 0;
-  player.image = player.sprites.idle.image;
 
   if (keys.d.pressed && player.lastKey === "d") {
     player.velocity.x = 5;
-    player.image = player.sprites.run.image;
+    player.switchSprite("run");
   } else if (keys.a.pressed && player.lastKey === "a") {
     player.velocity.x = -5;
-    player.image = player.sprites.run.image;
+    player.switchSprite("run");
+  } else {
+    player.switchSprite("idle");
+  }
+
+  if (player.velocity.y < 0) {
+    player.switchSprite("jump");
+  } else if (player.velocity.y > 0) {
+    player.switchSprite("fall");
   }
 
   //enemy movement
