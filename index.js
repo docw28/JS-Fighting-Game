5189319a3c5a16edad1d2d0560e6a185c4125756
docw28 <d.carverwilliams@gmail.com>
2022-04-29 01:19:31 +0100

    Added HUD

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ class Sprite {
     };
     this.colour = colour;
     this.isAttacking;
+    this.health = 100;
   }
 
   draw() {
@@ -150,7 +151,8 @@ function animate() {
     && player.isAttacking
   ) {
     player.isAttacking = false;
-    console.log(">stab noise<");
+    enemy.health -= 20;
+    document.querySelector("#enemyHealth").style.width = enemy.health + "%";
   }
 
   if (
@@ -161,7 +163,8 @@ function animate() {
     && enemy.isAttacking
   ) {
     enemy.isAttacking = false;
-    console.log(">noise of stab<");
+    player.health -= 20;
+    document.querySelector("#playerHealth").style.width = player.health + "%";
   }
 }
 
